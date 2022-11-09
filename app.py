7056024812ca26f197f6008296070b7a2e40041a
Tabish N <1631995@gmail.com>
2022-11-09 14:38:58 +0100

    voice detect and random word generator

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import json
+import random
 
 import rijmwoord
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -17,16 +16,36 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.route('/random_word')
+def random_word():
+    # rijmwoorden_dict = rijmwoord.rijmwoordenboek
+    # random_word = random.sample(random.choice(list(rijmwoorden_dict.values())), 1)[0]
+    # return random_word
+
+    with open("wordlist.txt", "r") as f:
+        random_word = random.sample(f.readlines(), 1)[0]
+
+    return random_word
+
+
 @app.route('/rhyme', methods=['POST'])
 def rhyme_word():
     data = request.form["query"].lower().strip().split()
+
+    if not data:
+        return "Geen rijmwoorden gevonden!"
+
     word = data[0]
 
     try:
-        rijm_embed = rijmwoord.rijmwoorden(word)
-        return json.dumps(list(rijm_embed))
+        rijm_embed = list(rijmwoord.rijmwoorden(word))
+
+        if not rijm_embed:
+            return "Geen rijmwoorden gevonden!"
+        else:
+            return jsonify(rijm_embed)
+
     except KeyError as e:
-        print(e)
         return "Geen rijmende woorden gevonden!"
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,76 @@
-// const SpeechRecogniton = window.SpeechRecognition || webkitSpeechRecognition;
-function startRecognition(){
-    console.log("Hello World!")
+const SpeechRecogniton = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecogniton();
+
+recognition.lang = 'nl-NL';
+recognition.continous = true;
+recognition.interimResults = true;
+recognition.maxAlternatives = 10;
+
+function startRecognition() {
+    recognition.start()
+    document.getElementById("speech-output").innerHTML = "";
+}
+
+let final_result;
+recognition.onresult = (event) => {
+    if (event.results !== undefined) {
+
+        let top_result = event.results[0][0].transcript;
+        document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><i>GEDETECTEERD: " + top_result + "</i></li>");
+        final_result = top_result;
+    }
+}
+
+recognition.onstart = (event) => {
+    document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><strong>Opname gestart!</strong></li>");
+}
+
+recognition.onspeechstart = (event) => {
+    document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><strong>Stem gedetecteerd!</strong></li>");
+
+}
+
+recognition.onspeechend = (event) => {
+    document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><strong>Stem niet meer gedetecteerd!</strong></li>");
+
+}
+
+recognition.onend = (event) => {
+    document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><strong>Opname gestopt!</strong></li>");
+    document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><i>Wat rijmt er op <strong>" + final_result + "</strong>?</i></li>");
+
+    let formData = new FormData();
+    formData.append('query', final_result);
+    fetch('/rhyme', {method: 'POST', body: formData})
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li><i>Dit rijmt op <strong>" + final_result + "</strong>:</i></li>");
+            for (let i = 0; i < data.length; i++) {
+                document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li>" + data[i] + "</li>");
+            }
+        })
+        .catch(error => {
+            document.getElementById("speech-output").insertAdjacentHTML("beforeend", "<li>Geen rijmende woorden gevonden </li>");
+        });
+
+    final_result = "";
+}
+
+document.getElementById("wotd-button").onclick = () => {
+    let wotd = document.getElementById("wotd").innerHTML;
+    let formData = new FormData();
+    formData.append('query', wotd);
+    fetch('/rhyme', {method: 'POST', body: formData})
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("wotd-result").insertAdjacentHTML("beforeend", "<li><i>Dit rijmt op <strong>" + wotd
+                + "</strong>:</i></li>");
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i])
+                document.getElementById("wotd-result").insertAdjacentHTML("beforeend", "<li>" + data[i] + "</li>");
+            }
+        })
+        .catch(error => {
+            document.getElementById("wotd-result").insertAdjacentHTML("beforeend", "<li>Geen rijmende woorden gevonden </li>");
+        });
 }
